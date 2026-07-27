3664abc5020d1e5fece0b86b71d231e4aabafbc6
Abdullox <you@example.com>
2026-07-27 17:08:49 +0500

    selection sort

---
+++ leetcode/medium/selectionsort/selectionSort.py
@@ -0,0 +1,27 @@
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        nums ro'yxatini joyida (in-place) saralaydi.
+        """
+        counts = [0, 0, 0]
+
+        for color in nums:
+            counts[color] += 1
+
+        R, W, B = counts
+        nums[:R] = [0] * R
+        nums[R:R + W] = [1] * W
+        nums[R + W:] = [2] * B
+
+
+# Test
+if __name__ == "__main__":
+    nums = [2, 0, 2, 1, 1, 0]
+
+    solution = Solution()
+    solution.sortColors(nums)
+
+    print(nums)
